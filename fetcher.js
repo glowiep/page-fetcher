804ff219b828d648e288dc0c58d5783d620e6f71
Gloria Lim <glorialimartt@gmail.com>
2023-10-16 23:43:27 -0400

    Added fetcher.js

---
+++ fetcher.js
@@ -0,0 +1,12 @@
+const args = process.argv.slice(2);
+const url = args[0];
+const filePath = args[1];
+
+console.log(url)
+
+// make an http request and wait for the response.
+
+// After the http request is complete, take the data you receive and write it to a file in your local filesystem.
+
+// When you're trying to control the order of asynchronous operations, you can use nested callbacks.
+
